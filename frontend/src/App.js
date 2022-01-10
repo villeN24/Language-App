e1632a208a9b002186b668005122e88f8ee7fea6
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-01-10 10:52:29 +0200

    Update visuals.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import StudentComponent from "./components/StudentComponent";
 import TeacherComponent from "./components/TeacherComponent";
+import Button from "@mui/material/Button";
 
 const App = () => {
   return (
     <div className="App">
       <BrowserRouter>
-        <Link to="/">Home</Link>
-        <Link to="/student">Student</Link>
-        <Link to="/teacher">teacher</Link>
+        <Link to="/student"><Button variant="outlined">Student</Button></Link>
+        <Link to="/teacher"><Button variant="outlined">Teacher</Button></Link>
         <Routes>
           <Route path="/student" element={<StudentComponent/>} />
           <Route path="/teacher" element={<TeacherComponent/>} />     
