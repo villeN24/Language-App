9232ac2d43f32c85db0be060f7e8cad07dabff97
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-01-12 13:41:30 +0200

    Move BrowserRouter to index to reduce clutter.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-import React, { useEffect, useState } from "react";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import StudentComponent from "./components/StudentComponent";
 import TeacherComponent from "./components/TeacherComponent";
 import Button from "@mui/material/Button";
@@ -14,40 +14,32 @@ const App = () => {
 
   return (
     <div className="App">
-      <BrowserRouter>
-        {disp ? (
-          <div>
-            <Link to="/student">
-              <div className="TopLinks">
-                <Button
-                  variant="outlined"
-                  size="large"
-                  onClick={() => setDisp(false)}
-                >
-                  Student
-                </Button>
-              </div>
-            </Link>
-            <Link to="/teacher">
-              <div className="TopLinks">
-                <Button variant="outlined" onClick={() => setDisp(false)}>
-                  Teacher
-                </Button>
-              </div>
-            </Link>
-          </div>
-        ) : null}
-        <Routes>
-          <Route
-            path="/student"
-            element={<StudentComponent setVis={setVis} />}
-          />
-          <Route
-            path="/teacher"
-            element={<TeacherComponent setVis={setVis} />}
-          />
-        </Routes>
-      </BrowserRouter>
+      {disp ? (
+        <div>
+          <Link to="/student">
+            <div className="TopLinks">
+              <Button
+                variant="outlined"
+                size="large"
+                onClick={() => setDisp(false)}
+              >
+                Student
+              </Button>
+            </div>
+          </Link>
+          <Link to="/teacher">
+            <div className="TopLinks">
+              <Button variant="outlined" onClick={() => setDisp(false)}>
+                Teacher
+              </Button>
+            </div>
+          </Link>
+        </div>
+      ) : null}
+      <Routes>
+        <Route path="/student" element={<StudentComponent setVis={setVis} />} />
+        <Route path="/teacher" element={<TeacherComponent setVis={setVis} />} />
+      </Routes>
     </div>
   );
 };
