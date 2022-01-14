c24c49be4d7420120e44fa8d69cca51cfab00bcb
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-01-14 14:59:36 +0200

    Add commenting to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,15 @@
+//@ts-check
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+/**
+ * @author Ville Naalisvaara <ville.naalisvaara@tuni.fi>
+ * @version 14.01.2022
+ */
 
 ReactDOM.render(
   <React.StrictMode>
