3e75d52a9235c1e546f572a18665a480c912c2bd
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-01-14 14:58:57 +0200

    Add commenting to App.js.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,15 @@ import StudentComponent from "./components/StudentComponent";
 import TeacherComponent from "./components/TeacherComponent";
 import HomeComponent from "./components/HomeComponent";
 
+/**
+ * The main component of the project.
+ *
+ * Includes routes in order to simulate MPA application
+ * and to divide the app in different pages. Everything
+ * is wrapped inside a <ThemeProvider> a component, to
+ * allow every component inside the project to use a
+ * custom material.ui theme.
+ */
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
