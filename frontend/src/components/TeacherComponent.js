d5935016e72f99753e09a3a2f4e6ea24df1bbda4
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-01-14 15:03:36 +0200

    Add commenting.

--- frontend/src/components/TeacherComponent.js
+++ frontend/src/components/TeacherComponent.js
@@ -1,9 +1,19 @@
+//@ts-check
 import React from "react";
 import TableComponent from "./TableComponent";
 import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 const styleVariant = "contained";
 
+/**
+ * A teacher view component.
+ *
+ * A teacher view component, that decides
+ * if the child component is in admin mode
+ * or not.
+ *
+ * @returns - A tablelist component in admin mode.
+ */
 function TeacherComponent(props) {
   return (
     <div>
