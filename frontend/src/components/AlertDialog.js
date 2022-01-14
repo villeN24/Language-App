9f7a12bb05a2175efe5e2b403a87c667616a7196
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-01-14 15:00:07 +0200

    Add commenting in AlertDialog.js.

--- frontend/src/components/AlertDialog.js
+++ frontend/src/components/AlertDialog.js
@@ -1,3 +1,4 @@
+//@ts-check
 import React, { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import Dialog from "@mui/material/Dialog";
@@ -6,17 +7,39 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import DialogTitle from "@mui/material/DialogTitle";
 
+/**
+ * A component to confirm deletion of a row.
+ *
+ * Opens an alert to user´s screen, with options
+ * to confirm, or cancel deletion.
+ */
 export default function AlertDialog(props) {
-  const [open, setOpen] = React.useState(props.display);
+  /** A boolean to control if the alert is visible. */
+  const [open, setOpen] = useState(props.display);
 
+  /**
+   * Opens the component at the start.
+   */
   useEffect(() => {
     setOpen(props.display);
   }, [props.display]);
-
+  /**
+   * Closes the alert and does nothing.
+   *
+   * Hides the alert when cancel is pressed
+   * on the alert popup.
+   */
   const handleCancel = () => {
     setOpen(false);
     props.stopDisplay();
   };
+  /**
+   * Closes the alert and confirms deletion.
+   *
+   * Hides the alert when confirm is pressed, and
+   * calls a parent method to delete the row from
+   * the list.
+   */
   const handleConfirm = () => {
     setOpen(false);
     props.deleteItem();
