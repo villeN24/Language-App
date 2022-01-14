f029f3f31ab77f319ddd0060545e7ea9fa9295d2
Ville Naalisvaara <ville.naalisvaara@tuni.fi>
2022-01-14 15:10:20 +0200

    Add commenting and cleanup.

--- database/functions.js
+++ database/functions.js
@@ -1,5 +1,6 @@
 const mysql = require("mysql");
 
+/** An object to create a connection to database. */
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
@@ -8,6 +9,7 @@ const connection = mysql.createConnection({
 });
 
 let connectionFunctions = {
+  /** A function to establish a connection to a database. */
   connect: () => {
     function funkkari(resolve, reject) {
       connection.connect((err) => {
@@ -20,6 +22,7 @@ let connectionFunctions = {
     }
     return new Promise(funkkari);
   },
+  /** A function to close the connection to the database */
   close: () => {
     function funkkari(resolve, reject) {
       connection.end((err) => {
@@ -32,12 +35,20 @@ let connectionFunctions = {
     }
     return new Promise(funkkari);
   },
+  /**
+   * A function to save a new entry to the database table.
+   *
+   * A function to add a new row to a database table with
+   * given parameters, with character escaping.
+   *
+   * @param {string} finnish - Value to be inserted to the finnish column.
+   * @param {string} english - Value to be inserted to the english column.
+   * @param {string} swedish - Value to be inserted to the swedish column.
+   * @param {string} category - Value to be inserted to the category column.
+   * @returns - A promise to allow async syntax
+   */
   save: (finnish, english, swedish, category) => {
     function funkkari(resolve, reject) {
-      console.log(
-        `[LOG] Inserting fin:${finnish} eng:${english} swe:${swedish} category:${category}`
-      );
-      console.log(finnish, english, category);
       connection.query(
         `INSERT INTO dictionary (finnish, english, swedish, category) Values(?, ?, ?, ?)`,
         [finnish, english, swedish, category],
@@ -52,9 +63,12 @@ let connectionFunctions = {
     }
     return new Promise(funkkari);
   },
+  /**
+   * A function to get everything from a database table.   *
+   * @returns - A promise to allow async syntax.
+   */
   findAll: () => {
     function funkkari(resolve, reject) {
-      console.log(`[LOG] Finding all`);
       connection.query(`SELECT * FROM dictionary`, (err, allLocations) => {
         if (err) {
           reject(err);
@@ -82,9 +96,17 @@ let connectionFunctions = {
     }
     return new Promise(funkkari);
   },
+  /**
+   * A function to delete a row from a database table.
+   *
+   * Deletes a certain row from a database table with
+   * given id, includes character escaping.
+   *
+   * @param {number} id - The value of the row to be deleted.
+   * @returns - A promise to allow async syntax.
+   */
   deleteById: (id) => {
     function funkkari(resolve, reject) {
-      console.log(`[LOG] Deleting id:${id}`);
       connection.query(`DELETE FROM dictionary WHERE id = ?`, [id], (err) => {
         if (err) {
           reject(err);
@@ -95,9 +117,16 @@ let connectionFunctions = {
     }
     return new Promise(funkkari);
   },
+  /**
+   * A function to find a row from a database table.
+   *
+   * Finds a certain row from a database table with given
+   * id, and returns it.
+   * @param {number} id - The value of the row to be found.
+   * @returns - A promise to allow async syntax.
+   */
   findById: (id) => {
     function funkkari(resolve, reject) {
-      console.log(`[LOG] Finding id ${id}`);
       connection.query(
         `SELECT * FROM dictionary WHERE id = ?`,
         [id],
@@ -114,9 +143,22 @@ let connectionFunctions = {
     }
     return new Promise(funkkari);
   },
+  /**
+   * A function to edit the values of a certain row of a database table.
+   *
+   * Edits a certain row from a database table with
+   * given id and values. Includes character
+   * escaping.
+   *
+   * @param {number} id - Value of the row to be edited.
+   * @param {string} finnish - Value to be inserted to the finnish column.
+   * @param {string} english - Value to be inserted to the finnish column.
+   * @param {string} swedish - Value to be inserted to the finnish column.
+   * @param {string} category - Value to be inserted to the finnish column.
+   * @returns - A promise to allow async syntax.
+   */
   editEntry: (id, finnish, english, swedish, category) => {
     function funkkari(resolve, reject) {
-      console.log(`[LOG] Editing entry ${id}`);
       connection.query(
         `UPDATE dictionary SET finnish = ?, english = ?, swedish = ?, category= ? WHERE id = ?;`,
         [finnish, english, swedish, category, id],
@@ -131,9 +173,16 @@ let connectionFunctions = {
     }
     return new Promise(funkkari);
   },
+  /**
+   * A function to get all unique categories.
+   *
+   * Gets all unique categories from a database
+   * table, and returns them.
+   *
+   * @returns - A promise to allow async syntax.
+   */
   getCategories: () => {
     function funkkari(resolve, reject) {
-      console.log(`[LOG] Finging unique categories`);
       connection.query(
         `SELECT distinct category FROM dictionary;`,
         (err, allCategories) => {
